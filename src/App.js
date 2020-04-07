cb2b4b506d40a45884c155c580209d21451e6594
Honza Pardubický <jan.pardubicky@gmail.com>
2020-04-07 23:57:18 +0200

    Nastavit document.title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Navigation from "./Navigation";
 import AppBody from "./AppBody";
 import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
+
+    useEffect(() => {
+        document.title = "Litomericko API Client"
+    }, []);
+
     return (
         <Router>
             <Navigation/>
