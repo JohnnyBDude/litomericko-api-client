af1c28b867f5d122cc70bfb3120432f6fefe8e04
Honza Pardubický <jan.pardubicky@gmail.com>
2020-04-07 23:29:29 +0200

    fixup

---
+++ src/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import CompanyCategoriesDropdown from "./CompanyCategoriesDropdown";
+
+function Navigation() {
+  return (
+      <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">Litomericko API client</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                  <CompanyCategoriesDropdown/>
+              </Nav>
+              <Form inline>
+                  <FormControl type="text" placeholder="Hledej" className="mr-sm-2" />
+                  <Button variant="outline-success">Hledej</Button>
+              </Form>
+          </Navbar.Collapse>
+      </Navbar>
+  );
+}
+
+export default Navigation;
---
+++ src/data/mockData/companyCategories.js
@@ -0,0 +1,84 @@
+const companyCategories = [
+  {
+    id: 1,
+    title: 'Obchody a služby',
+    slug: 'obchody-a-sluby'
+  },
+  {
+    id: 2,
+    title: 'Obchodní řetězce',
+    slug: 'obchodni-retezce'
+  },
+  {
+    id: 3,
+    title: 'Stavebniny',
+    slug: 'stavebniny'
+  },
+  {
+    id: 4,
+    title: 'Ubytování, stravování, občerstvení',
+    slug: 'stravovani_obcerstveni'
+  },
+  {
+    id: 5,
+    title: 'Maloobchody',
+    slug: 'maloobchody'
+  },
+  {
+    id: 7,
+    title: 'Finanční služby',
+    slug: 'financni-sluzby'
+  },
+  {
+    id: 8,
+    title: 'Služby, řemesla',
+    slug: 'sluzby_remesla'
+  },
+  {
+    id: 9,
+    title: 'Elektronika',
+    slug: 'elektronika'
+  },
+  {
+    id: 10,
+    title: 'Instituce',
+    slug: 'instituce'
+  },
+  {
+    id: 11,
+    title: 'Zdravotnictví',
+    slug: 'zdravotnictvi'
+  },
+  {
+    id: 12,
+    title: 'Velkoobchody',
+    slug: 'velkoobchody'
+  },
+  {
+    id: 13,
+    title: 'Domácnost',
+    slug: 'domacnost'
+  },
+  {
+    id: 14,
+    title: 'Zahrada',
+    slug: 'zahrada'
+  },
+  {
+    id: 15,
+    title: 'Neziskove organizace',
+    slug: 'neziskove-organizace'
+  },
+  {
+    id: 16,
+    title: 'Ostatní',
+    slug: 'ostatni'
+  },
+  {
+    id: 17,
+    title: 'Školství ',
+    slug: 'skolstvi'
+  }
+];
+
+export default companyCategories
