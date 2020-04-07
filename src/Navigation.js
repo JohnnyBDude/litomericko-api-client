8680d031f7360464e85a8056ed57acd77f818dff
Honza Pardubický <jan.pardubicky@gmail.com>
2020-04-07 23:59:45 +0200

    Set version v0.0.1

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,7 +10,7 @@ import CompanyCategoriesDropdown from "./CompanyCategoriesDropdown";
 function Navigation() {
   return (
       <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Litomericko API client</Navbar.Brand>
+          <Navbar.Brand href="/">Litomericko API client v0.0.1</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
